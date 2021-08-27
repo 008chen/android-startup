09880433862ed0dd77399e6f9e5d5385f8625f50
Lin.Chen <MOSI000147@mosi-tech.com>
2021-08-27 17:24:29 +0800

    add file

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,16 @@ plugins {
 }
 
 repositories {
-    jcenter()
+//    jcenter()
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/google/")
+    }
+    maven { setUrl("https://jitpack.io") }
+    maven { setUrl("http://maven.aliyun.com/nexus/content/groups/public/") }
+    maven { setUrl("http://maven.aliyun.com/nexus/content/repositories/jcenter") }
+    maven { setUrl("http://maven.aliyun.com/nexus/content/repositories/google") }
+    maven { setUrl("http://maven.aliyun.com/nexus/content/repositories/gradle-plugin") }
 }
